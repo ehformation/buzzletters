c093dd9b0aa7c942909bcc6cfbeadce4939ea6d9
eliewebavenue <elie@webavenue.agency>
2024-09-24 14:15:54 +0200

    add to inscription

--- inscription.php
+++ inscription.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buzzletters - Inscris toi à notre newsletters</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php 
