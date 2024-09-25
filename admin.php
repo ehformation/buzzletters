863e8c0b7ccf0ea05bc11be226ef4dd21451c225
eliewebavenue <elie@webavenue.agency>
2024-09-25 16:23:16 +0200

    add search

--- admin.php
+++ admin.php
@@ -35,9 +35,22 @@
             }
 
         }
+
+        if(isset($_GET["search"])){
+            $s = $_GET["s"];
+
+            require 'connect.php';
+            $search_result = $connexion->query("SELECT * FROM subscribers WHERE email LIKE '%$s%' ");
+        }
     ?>
     <div class="container">
         <h1>Buzzletters - Admin</h1>
+
+        <form action="" method="GET">
+            <input type="text" name="s" placeholder="Ex: j.doe@gmail.com">
+            <button type="submit" name="search">Rechercher</button>
+            <button type="submit" name="search">Renitialiser</button>
+        </form>
 
         <table border="1">
             <tr>
@@ -48,36 +61,52 @@
             </tr>
 
             <?php
-                /* Etape 1 : Connexion a la base de données buzzletters */
-                require "connect.php";
+                if(isset($search_result) && $search_result->num_rows > 0 ) {
+                   /* Etape 3 : Affichage du resultat recherché */
+                    while( $subscriber = $search_result->fetch_assoc() ){
+                        $id = $subscriber["id"];
+                        $class = getClassByTheme($subscriber["theme"]);
 
-                /* Etape 2 : Requete pour récuperer les données */
-                $result = $connexion->query("SELECT * FROM subscribers");
+                        echo "<tr>"; 
+                            echo "<td>" . $subscriber["email"] . "</td>";
+                            echo "<td><div class='$class' >" . $subscriber["theme"] . "</div></td>";
+                            echo "<td>" . $subscriber["age"] . "</td>";
+                            echo "<td><a href='?delete&id=$id'>désinscrire</a></td>";
+                        echo "<tr>";
 
-                /* Etape 3 : Affichage du resultat */
-                while( $subscriber = $result->fetch_assoc() ){
-                    /**
-                     * 1er passage: 
-                     *  $subscriber =   Clé     | Valeur 
-                     *                  id      | 15
-                     *                  email   | john@free.fr
-                     *                  theme   | loisir 
-                     *                 
-                     * 2eme passage     Clé     | Valeur 
-                     *                  id      | 16
-                     *                  email   | richard@gmail.com
-                     *                  theme   | animaux 
-                    */
-                    $id = $subscriber["id"];
-                    $class = getClassByTheme($subscriber["theme"]);
+                    } 
+                }else{
+                    /* Etape 1 : Connexion a la base de données buzzletters */
+                    require "connect.php";
 
-                    echo "<tr>"; 
-                        echo "<td>" . $subscriber["email"] . "</td>";
-                        echo "<td><div class='$class' >" . $subscriber["theme"] . "</div></td>";
-                        echo "<td>" . $subscriber["age"] . "</td>";
-                        echo "<td><a href='?delete&id=$id'>désinscrire</a></td>";
-                    echo "<tr>";
+                    /* Etape 2 : Requete pour récuperer les données */
+                    $result = $connexion->query("SELECT * FROM subscribers");
 
+                    /* Etape 3 : Affichage du resultat */
+                    while( $subscriber = $result->fetch_assoc() ){
+                        /**
+                         * 1er passage: 
+                         *  $subscriber =   Clé     | Valeur 
+                         *                  id      | 15
+                         *                  email   | john@free.fr
+                         *                  theme   | loisir 
+                         *                 
+                         * 2eme passage     Clé     | Valeur 
+                         *                  id      | 16
+                         *                  email   | richard@gmail.com
+                         *                  theme   | animaux 
+                        */
+                        $id = $subscriber["id"];
+                        $class = getClassByTheme($subscriber["theme"]);
+
+                        echo "<tr>"; 
+                            echo "<td>" . $subscriber["email"] . "</td>";
+                            echo "<td><div class='$class' >" . $subscriber["theme"] . "</div></td>";
+                            echo "<td>" . $subscriber["age"] . "</td>";
+                            echo "<td><a href='?delete&id=$id'>désinscrire</a></td>";
+                        echo "<tr>";
+
+                    }
                 }
                 
             ?>
