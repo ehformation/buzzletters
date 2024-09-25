a9cfd9f41e0b6b286351ec0281ee5be5ad5945dd
eliewebavenue <elie@webavenue.agency>
2024-09-25 14:46:29 +0200

    add functions.php

--- inscription.php
+++ inscription.php
@@ -10,27 +10,7 @@
     <div class="form-inscription">
 
         <?php 
-            /*
-            La fonction isValidEmail($email) valide une adresse email en utilisant une expression régulière. Elle définit un modèle ($pattern) qui décrit le format acceptable d'une adresse email, incluant des caractères alphanumériques, le symbole "@", et un domaine suivi d'une extension. La fonction utilise preg_match() pour comparer l'email fourni avec le modèle défini. Si l'email correspond au modèle, elle retourne true, sinon elle retourne false, indiquant si l'email est valide ou non. 
-            */
-            function isValidEmail($email) {
-                $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
-                return preg_match($pattern, $email) === 1;
-            }
-
-            function emailExist($email){
-                /* Etape 1 : Connexion a la base de données buzzletters */
-                require "connect.php";
-
-                /* Etape 2 : Requete pour rechercher un email*/
-                $result = $connexion->query("SELECT * FROM subscribers WHERE email = '$email'");
-
-                if ($result->num_rows > 0) {
-                    return true; // L'email existe
-                } else {
-                    return false; // L'email n'existe pas
-                }
-            }
+            require 'functions.php';
 
             // Vérifie si le formulaire d'inscription a été soumis
             if( isset($_POST["inscription"]) ){
@@ -63,9 +43,6 @@
                                     echo "<p class='alert alert-error'>Une erreur est survenue, Veuillez réessayer</p>";
                                 }
                             }
-
-                            
-
                         }else {
                             echo "<p class='alert alert-error'>L'email n'est pas valide</p>";
                         }
