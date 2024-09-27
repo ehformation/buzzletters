11b740fa2f4389e83a58a57b2837f0d32ea99605
eliewebavenue <elie@webavenue.agency>
2024-09-27 14:34:17 +0200

    ajout des verification de connexion sur les page annexe (modification et connexion)

--- connexion.php
+++ connexion.php
@@ -8,38 +8,43 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="form-inscription">
-        <?php
-            if(isset($_POST["connexion"])){
-                $valid_login = "admin";
-                $valid_mdp = "1234";
+    <!-- Si il n'est PAS connecté  -->
+    <?php if(!isset($_SESSION["login"])) : ?>
+        <div class="form-inscription">
+            <?php
+                if(isset($_POST["connexion"])){
+                    $valid_login = "admin";
+                    $valid_mdp = "1234";
 
-                if(!empty($_POST["login"]) && !empty($_POST["mdp"])){
-                    $login = $_POST["login"];
-                    $mdp = $_POST["mdp"];
+                    if(!empty($_POST["login"]) && !empty($_POST["mdp"])){
+                        $login = $_POST["login"];
+                        $mdp = $_POST["mdp"];
 
-                    if($login == $valid_login && $mdp == $valid_mdp){
-                        $_SESSION["login"] = $login;
-                        header("Location: admin.php");
-                    }else{
-                        echo "<div class='alert alert-error'>Identifiant ou mot de passe incorrect</div>";
+                        if($login == $valid_login && $mdp == $valid_mdp){
+                            $_SESSION["login"] = $login;
+                            header("Location: admin.php");
+                        }else{
+                            echo "<div class='alert alert-error'>Identifiant ou mot de passe incorrect</div>";
+                        }
                     }
                 }
-            }
-        
-        ?>
+            
+            ?>
 
-    
-        <h1>Connectez-vous à l'admin Buzzletters</h1>
-        <form action="" method="post">
-            <div>
-                <input type="text" name="login" placeholder="Login">
-            </div>
-            <div>
-                <input type="password" name="mdp" placeholder="Mot de passe">
-            </div>
-            <button name="connexion" type="submit">Connexion</button>
-        </form>
-    </div>
+        
+            <h1>Connectez-vous à l'admin Buzzletters</h1>
+            <form action="" method="post">
+                <div>
+                    <input type="text" name="login" placeholder="Login">
+                </div>
+                <div>
+                    <input type="password" name="mdp" placeholder="Mot de passe">
+                </div>
+                <button name="connexion" type="submit">Connexion</button>
+            </form>
+        </div>
+    <?php else :  ?>
+        <p>Vous etes deja connecté</p>
+    <?php endif; ?>
 </body>
 </html>
--- update-subscriber.php
+++ update-subscriber.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,73 +8,78 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-    require 'connect.php';
-    require 'functions.php';
-    if(isset($_GET["id"])) :
 
-    $id = $_GET["id"];
-    $result = $connexion->query("SELECT * FROM subscribers WHERE id = $id");
-    /* Je recupere l'abonée depuis la bdd dont l'id est passé par l'url  */
-    $subscriber = $result->fetch_assoc();
+        <?php 
+    if(isset($_SESSION["login"])) : 
+        require 'connect.php';
+        require 'functions.php';
+        if(isset($_GET["id"])) :
 
-    /*
-    * Cle | valeur
-    * email | john@free.fr
-    * theme | loisir
-    * Age   | 23
-        */
-    if(isset($_POST['update'])){
-        if( !empty($_POST["email"]) && !empty($_POST["theme"]) && !empty($_POST["age"]) ){
-            $email = $_POST['email'];
-            $theme = $_POST['theme'];
-            $age = $_POST['age'];
+        $id = $_GET["id"];
+        $result = $connexion->query("SELECT * FROM subscribers WHERE id = $id");
+        /* Je recupere l'abonée depuis la bdd dont l'id est passé par l'url  */
+        $subscriber = $result->fetch_assoc();
 
-            if(isValidEmail($email)) {
+        /*
+        * Cle | valeur
+        * email | john@free.fr
+        * theme | loisir
+        * Age   | 23
+            */
+        if(isset($_POST['update'])){
+            if( !empty($_POST["email"]) && !empty($_POST["theme"]) && !empty($_POST["age"]) ){
+                $email = $_POST['email'];
+                $theme = $_POST['theme'];
+                $age = $_POST['age'];
 
-                $result = $connexion->query("UPDATE subscribers SET email = '$email', theme = '$theme', age = '$age' WHERE id = $id");
+                if(isValidEmail($email)) {
 
-                if($result){
-                    echo "<p class='alert alert-success'>Modifications effectuées</p>"; 
-                    header("Location: admin.php"); //redirection de la page vers admin.php
-                }else{
-                    echo "<p class='alert alert-error'>Une erreur est survenue, Veuillez réessayer</p>";
+                    $result = $connexion->query("UPDATE subscribers SET email = '$email', theme = '$theme', age = '$age' WHERE id = $id");
+
+                    if($result){
+                        echo "<p class='alert alert-success'>Modifications effectuées</p>"; 
+                        header("Location: admin.php"); //redirection de la page vers admin.php
+                    }else{
+                        echo "<p class='alert alert-error'>Une erreur est survenue, Veuillez réessayer</p>";
+                    }
+                    
+                }else {
+                    echo "<p class='alert alert-error'>L'email n'est pas valide</p>";
                 }
-                
             }else {
-                echo "<p class='alert alert-error'>L'email n'est pas valide</p>";
-            }
-        }else {
-            echo "<p class='alert alert-error'>Tous les champs sont requis</p>";
-        } 
-    }
-    
-    ?><div class="form-inscription">
-        <form action="?id=<?php echo $id ?>" method="POST">
-            <div>
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" required value="<?php echo $subscriber["email"] ?>">
-            </div>
-            <div>
-                <label for="age">Age</label>
-                <input type="text" name="age" id="age" required value="<?php echo $subscriber["age"] ?>">
-            </div>
-            <div>
-                <label for="theme">Thème</label>
-                <select name="theme" id="theme" required>
-                    <option value="loisir" <?php echo ($subscriber["theme"] == 'loisir') ? 'selected' : ''; ?>>Loisir</option>
-                    <option value="sport" <?php echo ($subscriber["theme"] == 'sport') ? 'selected' : ''; ?>>Sport</option>
-                    <option value="technologies" <?php echo ($subscriber["theme"] == 'technologies') ? 'selected' : ''; ?>>Technologies</option>
-                    <option value="animaux" <?php echo ($subscriber["theme"] == 'animaux') ? 'selected' : ''; ?>>Animaux</option>
-                    <option value="musique" <?php echo ($subscriber["theme"] == 'musique') ? 'selected' : ''; ?>>Musique</option>
-                    <option value="film" <?php echo ($subscriber["theme"] == 'loisir') ? 'film' : ''; ?>>Film</option>
-                </select>
-            </div>
-            <button type="submit" name="update" >Modifier</button>
-        </form>
-    </div>
+                echo "<p class='alert alert-error'>Tous les champs sont requis</p>";
+            } 
+        }
+        
+        ?><div class="form-inscription">
+            <form action="?id=<?php echo $id ?>" method="POST">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" required value="<?php echo $subscriber["email"] ?>">
+                </div>
+                <div>
+                    <label for="age">Age</label>
+                    <input type="text" name="age" id="age" required value="<?php echo $subscriber["age"] ?>">
+                </div>
+                <div>
+                    <label for="theme">Thème</label>
+                    <select name="theme" id="theme" required>
+                        <option value="loisir" <?php echo ($subscriber["theme"] == 'loisir') ? 'selected' : ''; ?>>Loisir</option>
+                        <option value="sport" <?php echo ($subscriber["theme"] == 'sport') ? 'selected' : ''; ?>>Sport</option>
+                        <option value="technologies" <?php echo ($subscriber["theme"] == 'technologies') ? 'selected' : ''; ?>>Technologies</option>
+                        <option value="animaux" <?php echo ($subscriber["theme"] == 'animaux') ? 'selected' : ''; ?>>Animaux</option>
+                        <option value="musique" <?php echo ($subscriber["theme"] == 'musique') ? 'selected' : ''; ?>>Musique</option>
+                        <option value="film" <?php echo ($subscriber["theme"] == 'loisir') ? 'film' : ''; ?>>Film</option>
+                    </select>
+                </div>
+                <button type="submit" name="update" >Modifier</button>
+            </form>
+        </div>
+        <?php else :  ?>
+            <p>Aucun abonée n'a été séléctionné</p>
+        <?php endif; ?>
     <?php else :  ?>
-        <p>Aucun abonée n'a été séléctionné</p>
+        <p>Vous ne pouvez pas accéder à cette page</p>
     <?php endif; ?>
 </body>
 </html>
